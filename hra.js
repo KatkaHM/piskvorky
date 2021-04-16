ed012b61765a5bb43b15ecd827b632eb918cfafa
Katka Martínková <martinkova.katie@gmail.com>
2021-04-16 19:08:19 +0200

    removed magic strings, extracted game logic into methods

--- hra.js
+++ hra.js
@@ -1,28 +1,38 @@
 'use strict';
-
-let soucasnyTah = 'circle';
+const circle = 'circle';
+const cross = 'cross';
+let soucasnyTah = circle;
 const elmPraveHraje = document.querySelector('.praveHraje');
 const elmHerniPole = document.querySelector('.herniPole');
+
+const zmenSoucasnyTah = () => {
+  if (soucasnyTah === circle) {
+    soucasnyTah = cross;
+    elmPraveHraje.src = `img/${cross}.svg`;
+  } else if (soucasnyTah === cross) {
+    soucasnyTah = circle;
+    elmPraveHraje.src = `img/${circle}.svg`;
+  } else {
+    console.log(
+      'something went wrong: wrong parameter for soucasnyTah:',
+      soucasnyTah,
+    );
+  }
+};
+
+const udelejTah = (element) => {
+  element.innerHTML = `<img src="img/${soucasnyTah}.svg" alt="${soucasnyTah}" class="marks--inGame" />`;
+  element.disabled = true;
+  zmenSoucasnyTah();
+};
 
 elmHerniPole.addEventListener('click', (event) => {
   console.log('isDisabled:', event.target.disabled);
 
   if (event.target.disabled === false) {
-    if (soucasnyTah === 'circle') {
-      console.dir(event);
-      event.target.innerHTML = `<img src="img/circle.svg" alt="circle" class="marks--inGame" />`;
-      event.target.disabled = true;
-      soucasnyTah = 'cross';
-      elmPraveHraje.src = 'img/cross.svg';
-    } else {
-      event.target.innerHTML = `<img src="img/cross.svg" alt="circle" class="marks--inGame" />`;
-      event.target.disabled = true;
-      soucasnyTah = 'circle';
-      elmPraveHraje.src = 'img/circle.svg';
-    }
+    udelejTah(event.target);
   } else {
     console.log('illegal move, nothing should happen');
     console.dir(event);
-    return;
   }
 });
