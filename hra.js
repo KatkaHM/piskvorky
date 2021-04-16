8d3c971872bb42796e28eb766110b03d0e5a484f
Katka Martínková <martinkova.katie@gmail.com>
2021-04-16 18:11:26 +0200

    js vkladani X/O do herni plochy

--- hra.js
+++ hra.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let soucasnyTah = 'circle';
+const elmPraveHraje = document.querySelector('.praveHraje');
+const elmHerniPole = document.querySelector('.herniPole');
+
+elmHerniPole.addEventListener('click', (event) => {
+  if (soucasnyTah === 'circle') {
+    console.dir(event);
+    event.target.innerHTML = `<img src="img/circle.svg" alt="circle" class="marks--inGame" />`;
+    soucasnyTah = 'cross';
+    elmPraveHraje.src = 'img/cross.svg';
+  } else {
+    event.target.innerHTML = `<img src="img/cross.svg" alt="circle" class="marks--inGame" />`;
+    soucasnyTah = 'circle';
+    elmPraveHraje.src = 'img/circle.svg';
+  }
+});
