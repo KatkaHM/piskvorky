3a7424c7f9bd2b18039a3e9bf314a085aa8dbc96
Katka Martínková <martinkova.katie@gmail.com>
2021-04-16 18:45:41 +0200

    fixed overwriting of already filled squares

--- hra.js
+++ hra.js
@@ -5,14 +5,24 @@ const elmPraveHraje = document.querySelector('.praveHraje');
 const elmHerniPole = document.querySelector('.herniPole');
 
 elmHerniPole.addEventListener('click', (event) => {
-  if (soucasnyTah === 'circle') {
-    console.dir(event);
-    event.target.innerHTML = `<img src="img/circle.svg" alt="circle" class="marks--inGame" />`;
-    soucasnyTah = 'cross';
-    elmPraveHraje.src = 'img/cross.svg';
+  console.log('isDisabled:', event.target.disabled);
+
+  if (event.target.disabled === false) {
+    if (soucasnyTah === 'circle') {
+      console.dir(event);
+      event.target.innerHTML = `<img src="img/circle.svg" alt="circle" class="marks--inGame" />`;
+      event.target.disabled = true;
+      soucasnyTah = 'cross';
+      elmPraveHraje.src = 'img/cross.svg';
+    } else {
+      event.target.innerHTML = `<img src="img/cross.svg" alt="circle" class="marks--inGame" />`;
+      event.target.disabled = true;
+      soucasnyTah = 'circle';
+      elmPraveHraje.src = 'img/circle.svg';
+    }
   } else {
-    event.target.innerHTML = `<img src="img/cross.svg" alt="circle" class="marks--inGame" />`;
-    soucasnyTah = 'circle';
-    elmPraveHraje.src = 'img/circle.svg';
+    console.log('illegal move, nothing should happen');
+    console.dir(event);
+    return;
   }
 });
